b6bf74dc0e246c8228764d3de90ea7b6aef13859
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-21 00:48:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "6883697340:AAGzWqsELZ9xgRFHEWgmJE5vUe4vCsmfGUo",
+             bot_token= "6883697340:AAEN5T5pgQPLEiVIcR2vURNeCxI-AgcVpPI",
              api_id= 20945078,
              api_hash= "93f6b8ce4bb0ab61b4c7e42187f2aa64")
 
