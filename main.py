023b4ca52615fdf9d9b31a6a10f52f5369d40267
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-02-20 16:47:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -189,7 +189,7 @@ async def account_login(bot: Client, m: Message):
                         download_cmd = f"{cmd} -R 25 --fragment-retries 25"
                         os.system(download_cmd)
                         copy = await bot.send_document(chat_id=m.chat.id,document=f'{name}.pdf', caption=cc1)
-                        await copy.copy(chat_id = -1002026611663)
+                        await copy.copy(chat_id = -1002057338886)
                         count += 1
                         os.remove(f'{name}.pdf')
                     except FloodWait as e:
