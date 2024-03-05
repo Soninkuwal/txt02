ccaa40a8da745c5303e02f498db08bb7aa4c5102
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-03-05 23:13:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -195,27 +195,6 @@ async def txt_handler(bot: Client, m: Message):
                        continue
                    except Exception as e:
                        print(f"Exception: {e}")
-
-
-                       download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
-                       global failed_counter
-                       print(download_cmd)
-                       logging.info(download_cmd)
-                       k = subprocess.run(download_cmd, shell=True)
-                     
-                   elif os.path.isfile(f"{name}.webm"):
-                       return f"{name}.webm"
-                       name = name.split(".")[0]
-                       if os.path.isfile(f"{name}.mkv"):
-                       return f"{name}.mkv"
-                  elif os.path.isfile(f"{name}.mp4"):
-                       return f"{name}.mp4"
-                  elif os.path.isfile(f"{name}.mp4.webm"):
-                       return f"{name}.mp4.webm"
-
-                       return name
-                       except FileNotFoundError as exc:
-                  return os.path.isfile.splitext[0] + "." + "mp4"
               
 
                 elif ".mp4" in url:
