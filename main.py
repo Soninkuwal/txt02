28613b77d0d303b4a374765da9a3a2cd39c639aa
Soninkuwal <158008154+Soninkuwal@users.noreply.github.com>
2024-03-05 22:56:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -196,21 +196,33 @@ async def txt_handler(bot: Client, m: Message):
                    except Exception as e:
                        print(f"Exception: {e}")
 
-                 elif "." in url:
-                   try:
-                       cmd = f'yt-dlp -o "{name}.mkv" "{url}"'
-                       download_cmd = f"{cmd} -R 25 --fragment-retries 25"
-                       os.system(download_cmd)
-                       await bot.send_video(chat_id=m.chat.id, video=f'{name}.mkv', caption=cc)
-                       count += 1
-                       os.remove(f'{name}.mkv')
-                   except FloodWait as e:
-                       print(f"FloodWait Exception: {e}")
-                       await m.reply_text(str(e))
-                       time.sleep(e.x)
-                       continue
-                   except Exception as e:
-                       print(f"Exception: {e}")
+                 async def download_video(url, cmd, name):
+                       download_cmd = f'{cmd} -R 25 --fragment-retries 25 --external-downloader aria2c --downloader-args "aria2c: -x 16 -j 32"'
+                       global failed_counter
+                       print(download_cmd)
+                       logging.info(download_cmd)
+                       k = subprocess.run(download_cmd, shell=True)
+                       if "visionias" in cmd and k.returncode != 0 and failed_counter <= 10:
+                       failed_counter += 1
+                       await asyncio.sleep(5)
+                       await download_video(url, cmd, name)
+                       failed_counter = 0
+                       try:
+                       if os.path.isfile(name):
+                       return name
+                   elif os.path.isfile(f"{name}.webm"):
+                       return f"{name}.webm"
+                       name = name.split(".")[0]
+                       if os.path.isfile(f"{name}.mkv"):
+                       return f"{name}.mkv"
+                  elif os.path.isfile(f"{name}.mp4"):
+                       return f"{name}.mp4"
+                  elif os.path.isfile(f"{name}.mp4.webm"):
+                       return f"{name}.mp4.webm"
+
+                       return name
+                       except FileNotFoundError as exc:
+                  return os.path.isfile.splitext[0] + "." + "mp4"
               
 
                 elif ".mp4" in url:
